0229ac2899ea3e653fb25a7b8fde110e921fd96e
rzepkamichal <michrzep@interia.pl>
2020-04-01 21:53:20 +0200

    Initialize project

---
+++ src/main/java/pl/umpa/UmpaApplication.java
@@ -0,0 +1,13 @@
+package pl.umpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/umpa/UmpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
